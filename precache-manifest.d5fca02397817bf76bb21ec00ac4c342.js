7874ce108e56d8fefaf4d9a007175d0412c4d239
animesh <animesh.ranchi91@gmail.com>
2019-10-04 18:42:58 +0530

    Updates

---
+++ precache-manifest.d5fca02397817bf76bb21ec00ac4c342.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae38d2faa360e92ddb724c34fec8f021",
+    "url": "/5WeatherApp/index.html"
+  },
+  {
+    "revision": "93141d3530fc63d5fd3c",
+    "url": "/5WeatherApp/static/css/main.262791bf.chunk.css"
+  },
+  {
+    "revision": "f02d646e6c059ac4e16b",
+    "url": "/5WeatherApp/static/js/2.c9133855.chunk.js"
+  },
+  {
+    "revision": "93141d3530fc63d5fd3c",
+    "url": "/5WeatherApp/static/js/main.e92f7166.chunk.js"
+  },
+  {
+    "revision": "f9a3a3611ff583e91e63",
+    "url": "/5WeatherApp/static/js/runtime~main.98356e61.js"
+  },
+  {
+    "revision": "a1cb48a64eeb44046ffc25f9be1e4a8a",
+    "url": "/5WeatherApp/static/media/clouds.a1cb48a6.png"
+  },
+  {
+    "revision": "afed36278400d3f0900ee004b8b7ee74",
+    "url": "/5WeatherApp/static/media/rain.afed3627.png"
+  },
+  {
+    "revision": "176637e3c6f8f35e7a3662a8896d8f19",
+    "url": "/5WeatherApp/static/media/sun.176637e3.png"
+  },
+  {
+    "revision": "782c0c0f8d97ef03d5294850253b34ec",
+    "url": "/5WeatherApp/static/media/sunny.782c0c0f.png"
+  }
+]);
